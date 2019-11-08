408dc3b844a90dcfdba96033cbec697b713eca88
Podo <cglee079@gmail.com>
2019-11-08 22:20:51 +0900

    set module

---
+++ itemwatcher-telegram/src/main/java/com/podo/itemwatchertelegram/ItemwatcherTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.podo.itemwatchertelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemwatcherTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemwatcherTelegramApplication.class, args);
+    }
+
+}
