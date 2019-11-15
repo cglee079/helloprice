5330c7b6f0ef7ced397b5a6e849452d4381aab62
Podo <cglee079@gmail.com>
2019-11-15 22:44:17 +0900

    update qa docker script

---
+++ helloprice-telegram/docker-build-script-qa.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd /volume1/docker-build/podo-helloprice/podo-helloprice-telegram-qa
+docker stop podo-helloprice-telegram-qa
+docker rm podo-helloprice-telegram-qa
+docker rmi podo-helloprice-telegram-qa:1.0
+docker build --tag podo-helloprice-telegram-qa:1.0 .
+docker run -it -v /volume1/docker/podo-helloprice/podo-helloprice-telegram-qa/logs:/data/logs  --name=podo-helloprice-telegram-qa podo-helloprice-telegram-qa:1.0
