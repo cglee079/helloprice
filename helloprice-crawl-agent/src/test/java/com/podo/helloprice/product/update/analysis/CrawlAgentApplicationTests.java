078529cd9f37523e9e8c0b1f6b4dc1d532842504
cglee079 <cglee079@zuminternet.com>
2020-04-22 15:32:49 +0900

    feature : crawl-agent deploy ready

--- helloprice-crawl-agent/src/test/java/com/podo/helloprice/product/update/analysis/CrawlAgentApplicationTests.java
+++ helloprice-crawl-agent/src/test/java/com/podo/helloprice/product/update/analysis/CrawlAgentApplicationTests.java
@@ -2,7 +2,9 @@ package com.podo.helloprice.product.update.analysis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CrawlAgentApplicationTests {
 
