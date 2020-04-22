82819cbf9716a4e17c5ad3269e49f5e1a08ce6a4
cglee079 <cglee079@zuminternet.com>
2020-04-22 13:47:14 +0900

    feature: 배포관련 설정

--- helloprice-crawl-scheduler/docker-build-script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /volume1/docker-build/helloprice/helloprice-crawl-scheduler
-docker stop helloprice-crawl-scheduler
-docker rm helloprice-crawl-scheduler
-docker rmi helloprice-crawl-scheduler:2.1
-docker build --tag helloprice-crawl-scheduler:2.1 .
-docker run -it -v /volume1/docker/helloprice/helloprice-crawl-scheduler/logs:/data/logs --name=helloprice-crawl-scheduler helloprice-crawl-scheduler:2.1
--- helloprice-telegram/docker-build-script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /volume1/docker-build/helloprice/helloprice-telegram
-docker stop helloprice-telegram
-docker rm helloprice-telegram
-docker rmi helloprice-telegram:2.0
-docker build --tag helloprice-telegram:2.0 .
-docker run -it -p 18080:8080 -p 25:25 -p 465:465 -p 587:587 -v /volume1/docker/helloprice/helloprice-telegram/logs:/data/logs  --name=helloprice-telegram helloprice-telegram:2.0
