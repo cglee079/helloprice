13dc02976c051fbf9a644f6f6504002e9af14e2d
cglee079 <cglee079@zuminternet.com>
2020-04-22 14:21:09 +0900

    feature : notifier deploy 준비

--- helloprice-notifier/src/test/java/com/podo/helloprice/NotifierApplicationTests.java
+++ helloprice-notifier/src/test/java/com/podo/helloprice/NotifierApplicationTests.java
@@ -1,8 +1,10 @@
-package com.podo.helloprice.product.update.analysis;
+package com.podo.helloprice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NotifierApplicationTests {
 
